4a9f30474ca29a1e039e8c2e1df8eacea70a9c7b
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-05-12 12:35:54 +0300

    + test

---
+++ 1test_CheckPointOperation.py
@@ -0,0 +1,33 @@
+import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Firefox()
+driver.get("http://dev.eor.gosapi.ru/")
+driver.maximize_window()
+wait = WebDriverWait(driver, 10)
+
+
+class ASeleniumLogin_1(unittest.TestCase):
+    def test_1LoginInEORDev(self):
+        assert "Login" in driver.title
+        _ = wait.until(EC.element_to_be_clickable((By.ID, 'LoginForm_username')))
+        elem = driver.find_element_by_id("LoginForm_username")
+        elem.send_keys("Ipad")
+        elem = driver.find_element_by_id("LoginForm_password")
+        elem.send_keys("ipad")
+        elem.send_keys(Keys.RETURN)
+    def test_2Not500or404andLoginIsVisible(self):
+        assert "500" not in driver.title  # проверка на 500/404 ошибку
+        assert "404" not in driver.title
+        _ = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'hidden-xs')))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
