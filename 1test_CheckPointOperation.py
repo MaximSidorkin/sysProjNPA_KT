eeaf16b7a3f4495e4cafe4422d0b2a2338351dd7
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-06-17 14:13:55 +0300

    + fix "Block search"

--- 1test_CheckPointOperation.py
+++ 1test_CheckPointOperation.py
@@ -67,12 +67,12 @@ class CSeleniumCreateNewCP(unittest.TestCase):
         elemSearch.click()
         elemSearch.send_keys('Selenium')
         elemSearch.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(4)
         assert "500" not in driver.title  # проверка на 500/404 ошибку
         assert "404" not in driver.title
     def test_2FindBlock(self):
         #находим блок
-        findBlock = driver.find_element_by_link_text('Создал Selenium _для редактирования edit')
+        findBlock = driver.find_element_by_link_text('Создал Selenium _для редактирования')
         findBlock.click()
         time.sleep(2)
         assert "500" not in driver.title  # проверка на 500/404 ошибку
