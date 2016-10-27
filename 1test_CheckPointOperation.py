e51cf81db985147aae63d79c9e56dc77c65e78e9
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-10-27 16:27:29 +0300

    + patch 2710 for 1test ++ MORE WAITING FOR GOD OF WAITING

--- 1test_CheckPointOperation.py
+++ 1test_CheckPointOperation.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 driver = webdriver.Firefox()
 driver.get("https://dev.eor.gosapi.ru/site/login")
 driver.maximize_window()
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 20)
 
 class ASeleniumLogin_1(unittest.TestCase):
     def test_001_LoginInEORDev(self):
