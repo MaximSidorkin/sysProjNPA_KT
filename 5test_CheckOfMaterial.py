e9118394c101b852bc6f52c01b0303c2e204f7e7
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-12-08 17:13:26 +0300

    + patch 2710 for 1test

--- 5test_CheckOfMaterial.py
+++ 5test_CheckOfMaterial.py
@@ -1,5 +1,7 @@
 import unittest
 import HTMLTestRunner
+import sys
+
 global str
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -26,35 +28,36 @@ class ASeleniumLogin_1(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         print('\n 1. Логинимся в систему')
         wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'hidden-xs')))
-'''
-    def test_002_GotoMaterial(self):
-        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'i.entypo-menu')))
-        driver.find_element_by_css_selector("i.entypo-menu").click()
-        wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Материалы')))
-        driver.find_element_by_link_text("Материалы").click()
-        _ = wait.until(EC.element_to_be_clickable((By.ID, 'search-show')))
-        print(' 2. Переходим в раздел "Материалы"')
+    sys.exit()
 
-    def test_003_FilterSetting(self):
-        driver.find_element_by_id('search-show').click()
-        driver.find_element_by_id('search-text').send_keys('1')
-        driver.find_element_by_id('search-text-push').click()
-        print(' 3. В поиске вводим "1"')
+    #def test_002_GotoMaterial(self):
+        #wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'i.entypo-menu')))
+        #driver.find_element_by_css_selector("i.entypo-menu").click()
+        #wait.until(EC.element_to_be_clickable((By.LINK_TEXT, 'Материалы')))
+        #driver.find_element_by_link_text("Материалы").click()
+        #_ = wait.until(EC.element_to_be_clickable((By.ID, 'search-show')))
+        #print(' 2. Переходим в раздел "Материалы"')
 
-    def test_004_SecondTry(self):
-        driver.find_element_by_css_selector('b.caret').click()
-        driver.find_element(By.XPATH,"// a[contains(text(), 'Не учитывать')]").click()
-        try:
-            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.find-text')))
-            driver.find_element_by_css_selector('span.find-text')
-            print(' 4. Материалы найден')
-        except:
-            self.fail(print(' 4. Материалы не найден'))
+    #def test_003_FilterSetting(self):
+    #    driver.find_element_by_id('search-show').click()
+    #    driver.find_element_by_id('search-text').send_keys('1')
+    #    driver.find_element_by_id('search-text-push').click()
+    #    print(' 3. В поиске вводим "1"')
 
-    def test_005_CloseDriver(self):
-        print(' 5. Тест завершен, браузер закрыт')
-        driver.close()
-'''
+    #def test_004_SecondTry(self):
+    #    driver.find_element_by_css_selector('b.caret').click()
+    #    driver.find_element(By.XPATH,"// a[contains(text(), 'Не учитывать')]").click()
+    #    try:
+    #        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.find-text')))
+    #        driver.find_element_by_css_selector('span.find-text')
+    #        print(' 4. Материалы найден')
+    #    except:
+    #        self.fail(print(' 4. Материалы не найден'))
+
+    #def test_005_CloseDriver(self):
+    #    print(' 5. Тест завершен, браузер закрыт')
+    #    driver.close()
+#'''
 if __name__ == '__main__':
     #unittest.main()
     suite = unittest.TestSuite()
@@ -67,8 +70,3 @@ if __name__ == '__main__':
     description='Отчет по тестированию'
     )
     runner.run(suite)
-
-if __name__ == '__main__':
-    exit()
-    #unittest.main()
-    #exit()
