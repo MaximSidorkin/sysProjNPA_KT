61949f84c38e500db8aad87cf2837844cba93bfe
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-12-09 13:45:34 +0300

    + patch 2710 for 1test

--- 5test_CheckOfMaterial.py
+++ 5test_CheckOfMaterial.py
@@ -28,7 +28,6 @@ class ASeleniumLogin_1(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         print('\n 1. Логинимся в систему')
         wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'hidden-xs')))
-        #sys.exit()
 
 if __name__ == '__main__':
         suite = unittest.TestSuite()
