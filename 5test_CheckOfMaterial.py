36305c62d74e0209bcbcd87cb6fad3f1d49842a6
MaximSidorkin <sidorkin.maxim@yahoo.com>
2016-12-09 12:07:25 +0300

    + patch 2710 for 1test

--- 5test_CheckOfMaterial.py
+++ 5test_CheckOfMaterial.py
@@ -8,10 +8,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+import sys
+
 driver = webdriver.Firefox()
 driver.get("https://dev.eor.gosapi.ru/site/login")
 driver.maximize_window()
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 20)
 driver.implicitly_wait(10)
 body = driver.find_element_by_tag_name('body')
 
@@ -26,14 +28,24 @@ class ASeleniumLogin_1(unittest.TestCase):
         elem.send_keys(Keys.RETURN)
         print('\n 1. Логинимся в систему')
         wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'hidden-xs')))
+        #sys.exit()
 #
+#if __name__ == '__main__':
+        #suite = unittest.TestSuite()
+        #suite.addTest(unittest.makeSuite(ASeleniumLogin_1))
+        #buf = open("at_for_MATERIAL.html", 'wb')
+        #runner = HTMLTestRunner.HTMLTestRunner(
+        #    stream=buf,
+        #    title='ПРОВЕРКА РАЗДЕЛА "МАТЕРИАЛЫ" НА ОТОБРАЖЕНИЕ МАТЕРИАЛОВ ПО ПОИСКУ БЕЗ УЧЁТА ВРЕМЕНИ',
+        #    description='Отчет по тестированию'
+        #   )
+        #runner.run(suite)
+
+        #if __name__ == "__main__":
+        #    unittest.main()
+        #ret = not runner.run(suite).wasSuccessful()
+        #sys.exit(ret)
+
 if __name__ == '__main__':
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(ASeleniumLogin_1))
-    buf = open("at_for_MATERIAL.html", 'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(
-    stream=buf,
-    title='ПРОВЕРКА РАЗДЕЛА "МАТЕРИАЛЫ" НА ОТОБРАЖЕНИЕ МАТЕРИАЛОВ ПО ПОИСКУ БЕЗ УЧЁТА ВРЕМЕНИ',
-    description='Отчет по тестированию'
-    )
-    runner.run(suite)
+    unittest.main()
+    #sys.exit()
